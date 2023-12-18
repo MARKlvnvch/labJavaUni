59a10d6464e8c119d7cf5227f8faae201800f617
MarkLevanovich999 <dalastexas999@gmail.com>
2023-12-18 23:13:26 +0600

    add lab4

--- lab4/src/main/java/functional/Segment.java
+++ lab4/src/main/java/functional/Segment.java
@@ -6,7 +6,7 @@ public class Segment <T extends Function> implements Functional<T>{
 
     @Override
     public double fun(T f) {
-        return f.fun(f.getStart()) + f.fun(f.getEnd()) + f.fun((f.getStart() + f.getEnd()) / 2);
+        return f.fun(f.getStart())  + f.fun(f.getEnd()) + f.fun((f.getStart() + f.getEnd()) / 2);
     }
 
 }
